93696c127215dc5d4583cbb04b9f7790500abb37
Marcelo Magalhães <marcelo.maga2550@gmail.com>
2021-10-15 09:38:25 -0300

    Initial commit

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  html, body, #root {
+    height: 100vh;
+  }
+  *, button, input {
+    border: 0;
+    outline: 0;
+    font-family: 'Raleway', sans-serif;
+  }
+  li {
+    list-style: none;
+  }
+  
+`
+
+export default GlobalStyles
---
+++ src/styles/theme.ts
@@ -0,0 +1,14 @@
+
+export const theme = {
+  colors: {
+    primary: '#03045E',
+    secondary: '#0077B6',
+
+    gray: '#D8D8D8',
+    white: '#F5F5F5',
+    black: '#000000',
+  },
+  fonts: {
+    
+  }
+}
